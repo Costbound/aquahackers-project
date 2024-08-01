2260c39feda58c345c54b4bcca6d894e72c3d888
Dmytro Popovskyi <popovskiy96@gmail.com>
2024-08-01 17:38:04 +0200

    create pages blanks

---
+++ src/pages/AquaTrackerPage/AquaTrackerPage.jsx
@@ -0,0 +1,9 @@
+import css from './AquaTrackerPage.module.css';
+
+function AquaTrackerPage(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default AquaTrackerPage;
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,9 @@
+import css from './HomePage.module.css'
+
+function HomePage(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default HomePage;
