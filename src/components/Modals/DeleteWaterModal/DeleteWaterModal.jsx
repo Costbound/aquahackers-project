49f23440940304595d343b233c858e49c9f02bb3
Anna01~ <verusiapavlovna@gmail.com>
2024-08-01 20:47:48 +0300

    add icon and styles

--- src/components/Modals/DeleteWaterModal/DeleteWaterModal.jsx
+++ src/components/Modals/DeleteWaterModal/DeleteWaterModal.jsx
@@ -1,24 +1,25 @@
 import css from "./DeleteWaterModal.module.css";
 // import toast from "react-hot-toast";
+import { AiOutlineClose } from "react-icons/ai";
 
 const DeleteWaterModal = () => {
   const handleSubmit = () => {};
 
   return (
     <div className={css.deleteModalContainer}>
+      <AiOutlineClose className={css.deleteModalCloseBtn} />
       <div className={css.deleteModalTextContainer}>
         <h2 className={css.deleteModalTitle}>Delete entry</h2>
         <p className={css.deleteModalText}>
           Are you sure you want to delete the entry?
         </p>
       </div>
-
-      <div className={css.btnWrap}>
-        <button className={css.btn} onClick={handleSubmit}>
+      <div className={css.deleteModalBtnWrapper}>
+        <button className={css.deleteModalBtn} onClick={handleSubmit}>
           Delete
         </button>
         <button
-          className={css.cancelBtn}
+          className={css.deleteModalCancelBtn}
           // onClick={}
         >
           Cancel
