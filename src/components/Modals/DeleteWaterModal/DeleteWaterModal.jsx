62b933d60dcaf9f4fdeaac56e2bd7a7c56252750
Anna01~ <verusiapavlovna@gmail.com>
2024-08-03 15:34:34 +0300

    add Modal component

--- src/components/Modals/DeleteWaterModal/DeleteWaterModal.jsx
+++ src/components/Modals/DeleteWaterModal/DeleteWaterModal.jsx
@@ -1,31 +1,32 @@
+import Modal from "../../Modal/Modal.jsx";
 import css from "./DeleteWaterModal.module.css";
-// import toast from "react-hot-toast";
 import { AiOutlineClose } from "react-icons/ai";
 
-const DeleteWaterModal = () => {
-  const handleSubmit = () => {};
+const DeleteWaterModal = ({ isOpen, onClose }) => {
+  const handleSubmit = () => {
+    onClose();
+  };
 
   return (
-    <div className={css.deleteModalContainer}>
-      <AiOutlineClose className={css.deleteModalCloseBtn} />
-      <div className={css.deleteModalTextContainer}>
-        <h2 className={css.deleteModalTitle}>Delete entry</h2>
-        <p className={css.deleteModalText}>
-          Are you sure you want to delete the entry?
-        </p>
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <div className={css.deleteModalContainer}>
+        <AiOutlineClose className={css.deleteModalCloseBtn} onClick={onClose} />
+        <div className={css.deleteModalTextContainer}>
+          <h2 className={css.deleteModalTitle}>Delete entry</h2>
+          <p className={css.deleteModalText}>
+            Are you sure you want to delete the entry?
+          </p>
+        </div>
+        <div className={css.deleteModalBtnWrapper}>
+          <button className={css.deleteModalBtn} onClick={handleSubmit}>
+            Delete
+          </button>
+          <button className={css.deleteModalCancelBtn} onClick={onClose}>
+            Cancel
+          </button>
+        </div>
       </div>
-      <div className={css.deleteModalBtnWrapper}>
-        <button className={css.deleteModalBtn} onClick={handleSubmit}>
-          Delete
-        </button>
-        <button
-          className={css.deleteModalCancelBtn}
-          // onClick={}
-        >
-          Cancel
-        </button>
-      </div>
-    </div>
+    </Modal>
   );
 };
 
