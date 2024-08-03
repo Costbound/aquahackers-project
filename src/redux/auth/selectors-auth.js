c8715496f8fd043ed0ed2685a69370dda983ec16
Aleksandr Pechak <alpechak@gmail.com>
2024-08-03 20:07:12 +0300

    update

--- src/redux/auth/selectors-auth.js
+++ src/redux/auth/selectors-auth.js
@@ -5,5 +5,3 @@ export const selectIsLoggedIn = state => state.auth.isLoggedIn;
 export const selectIsRefreshing = state => state.auth.isRefreshing;
 
 export const selectError = state => state.auth.error;
-
-export const selectUser = (state) =>state.user.user;
