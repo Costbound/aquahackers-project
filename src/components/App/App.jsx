ee8ad3e652fba4a2a6664d35f4fdd755e08d5683
Dmytro Popovskyi <popovskiy96@gmail.com>
2024-08-04 14:37:39 +0200

    update refresh functionality

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,11 +9,17 @@ import RestrictedRoute from "../RestrictedRoute/RestrictedRoute.jsx";
 import PrivateRoute from "../PrivateRoute/PrivateRoute.jsx";
 import AquaTrackerPage from "../../pages/AquaTrackerPage/AquaTrackerPage.jsx";
 import Loader from "../Loader/Loader.jsx";
+import {useEffect} from "react";
+import {refresh} from "../../redux/auth/ops-auth.js";
 
 
 export default function App() {
     const dispatch = useDispatch();
     const isRefreshing = useSelector(selectIsRefreshing);
+
+    useEffect(() => {
+        dispatch(refresh())
+    }, [dispatch])
 
     return isRefreshing ? (
         <Loader type="global" width="100" height="100"/>
