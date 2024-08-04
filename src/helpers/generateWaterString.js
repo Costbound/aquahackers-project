4657b66ddce66b8f39266b588925d435a8deb583
Dmytro Popovskyi <popovskiy96@gmail.com>
2024-08-04 11:50:38 +0200

    create generateWaterString function

--- src/helpers/generateWaterString.js
+++ src/helpers/generateWaterString.js
@@ -4,9 +4,5 @@ const generateWaterString = (waterAmount) => {
 
     return `${waterAmountConverted} ${metricUnits}`;
 }
-console.log(generateWaterString(1000))
-console.log(generateWaterString(1500))
-console.log(generateWaterString(1750))
-console.log(generateWaterString(700))
-console.log(generateWaterString(999))
+
 export default generateWaterString;
