6c1376b9358b2056d5cab413b0fe95f83de19446
unknown <tanish@ua.i>
2024-08-04 12:55:26 +0300

    up

---
+++ src/components/Page/Page.jsx
@@ -0,0 +1,16 @@
+import Logo from '../Logo/Logo';
+import css from './Page.module.css';
+import PropTypes from 'prop-types';
+const Page = ({ children }) => {
+  return (
+    <div className={css.page}>
+      <Logo />
+      {children}
+    </div>
+  );
+};
+Page.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Page;
