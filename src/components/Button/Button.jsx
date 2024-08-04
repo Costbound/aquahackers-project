e3594c43f984296932dd957398a84d2ef483c22c
Hennadii Polupan <polupan.gennadiy@gmail.com>
2024-08-04 23:59:23 +0100

    fix button styles

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,29 +1,24 @@
 import PropTypes from "prop-types";
+import css from "./Button.module.css";
+import clsx from "clsx";
 
-const Button = ({
-  children,
-  width,
-  height,
-  border,
-  backgroundColor,
-  color,
-  fontSize,
-  onClick,
-}) => {
+const Button = ({ children, width, height, onClick, styleType }) => {
   const buttonStyle = {
     width: width || "auto",
     height: height || "auto",
-    backgroundColor: backgroundColor || "#f0eff4",
-    border: border || "none",
-    borderRadius: "30px",
-    color: color || "#323f47",
-    cursor: "pointer",
-    fontFamily: "Poppins, sans-serif",
-    fontSize: fontSize || "16px",
   };
 
   return (
-    <button style={buttonStyle} onClick={onClick}>
+    <button
+      style={buttonStyle}
+      onClick={onClick}
+      className={clsx(css.button, {
+        [css.transparent]: styleType === "transparent",
+        [css.green]: styleType === "green",
+        [css.dark]: styleType === "dark",
+        [css.white]: styleType === "white",
+      })}
+    >
       {children}
     </button>
   );
@@ -33,11 +28,8 @@ Button.propTypes = {
   children: PropTypes.node.isRequired,
   width: PropTypes.string,
   height: PropTypes.string,
-  border: PropTypes.string,
-  color: PropTypes.string,
-  backgroundColor: PropTypes.string,
-  fontSize: PropTypes.string,
   onClick: PropTypes.func,
+  styleType: PropTypes.oneOf(["transparent", "dark", "green", "white"]),
 };
 
 export default Button;
