18e3c075873e40695e88e4ac6246cce8646be5a0
Lampdust <lampdust06@gmail.com>
2024-08-05 23:03:46 +0300

    upd

--- src/redux/modal/ops-modal.js
+++
@@ -1,8 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-
-export const openModal = createAsyncThunk(
-  "modal/openModal",
-  async (payload, thunkAPI) => {
-    return thunkAPI.fulfillWithValue(payload);
-  }
-);
--- src/redux/modal/selectors-modal.js
+++
@@ -1,3 +0,0 @@
-export const selectModalContent = (state) => state.modal.modalContent;
-
-export const selectIsModalOpen = (state) => state.modal.isOpen;
