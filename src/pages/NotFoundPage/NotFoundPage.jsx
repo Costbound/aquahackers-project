abafa1aa11375a5f897fa1c516be45911eba8962
Iryna-Pavlyk <145845654+Iryna-Pavlyk@users.noreply.github.com>
2024-08-05 22:22:06 +0300

    add NFP

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -3,12 +3,18 @@ import css from "./NotFoundPage.module.css";
 
 const NotFoundPage = () => {
   return (
-    <div className={css.wrap}>
-      <p className={css.txt}>Uh-oh...</p>
-      <p className={css.text}>Not Found Page. Please go to the</p>
-      <p className={css.link}>
-        <Link to="/">home page</Link>
-      </p>
+    <div className={css.wrapper}>
+      <div className={css.wrap}>
+        <p className={css.txt}>Uh-oh...</p>
+      </div>
+      <div className={css.wrap}>
+        <p className={css.text}>Not Found Page. Please go to the</p>
+        <p>
+          <Link className={css.link} to="/">
+            home page
+          </Link>
+        </p>
+      </div>
       <p className={css.status}>404</p>
     </div>
   );
