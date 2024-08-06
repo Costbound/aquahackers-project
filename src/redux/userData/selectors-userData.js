3fb85b34850e308e09ffaca50d8a2f55d4eebd0f
Lampdust <lampdust06@gmail.com>
2024-08-06 22:58:52 +0300

    upd

--- src/redux/userData/selectors-userData.js
+++ src/redux/userData/selectors-userData.js
@@ -1,11 +1,1 @@
-export const selectUser = (state) => state.user.user; //примерная логика чтоб я мог получать данные на имя, но это не точно тк это скорее относится к сеттингам
-
-export const selectName = (state) => state.user.name;
-
-export const selectGender = (state) => state.user.gender;
-
-export const selectWeight = (state) => state.user.weight;
-
-export const selectWaterRate = (state) => state.user.waterRate;
-
-export const selectSportTime = (state) => state.user.sportTime;
+export const selectUserName = (state) => state.userData.name;
