8826d8882c6cd745f96efe7072473f295d05ab80
Summershinne <145506570+Summershinne@users.noreply.github.com>
2024-08-06 21:28:58 +0300

    css

--- src/components/AddWaterBtn/AddWaterBtn.jsx
+++ src/components/AddWaterBtn/AddWaterBtn.jsx
@@ -1,14 +1,22 @@
 import css from './AddWaterBtn.module.css';
 import icon from '../../img/icons.svg';
-
+import Button from '../../components/Button/Button.jsx'
 
 export default function AddWaterBtn() {
     return (
-        <button className={css.button}>
-            <svg className={css.svgIconAdd}>
-                    <use href={`${icon}#icon-plus-1`}/>
-                </svg> 
-            Add water</button>
-        
+      
+        <Button type='button' width={'140px'} 
+        height={'46px'}
+            styleType={"dark"}>
+             <svg className={css.svgIconAdd}>
+                <use href={`${icon}#icon-plus-1`} />
+            </svg>Add water
+        </Button >
     )
 };
+
+  // <button className={css.button}>
+        //     <svg className={css.svgIconAdd}>
+        //             <use href={`${icon}#icon-plus-1`}/>
+        //         </svg> 
+        //     Add water</button>
--- src/components/WaterProgressBar/WaterProgressBar.jsx
+++ src/components/WaterProgressBar/WaterProgressBar.jsx
@@ -1,10 +1,46 @@
 import css from './WaterProgressBar.module.css';
 
-export default function WaterProgressBar () {
+export default function WaterProgressBar() {
+//      const progress = useSelector((state) => state.progressBar.progress);
+//   const dispatch = useDispatch();
+
+//   const fetchProgressData = async () => {
+//     // Це приклад запиту до API для отримання прогресу з бази даних
+//     const response = await fetch('https://api.example.com/progress');
+//     const data = await response.json();
+//     dispatch(setProgress(data.progress));
+//   };
+
+//   useEffect(() => {
+//     fetchProgressData();
+//   }, []);
+
+//   const updateProgress = (newProgress) => {
+//     dispatch(setProgress(newProgress));
+//   };
+
     return (
-        <div>
-            <h2>Today</h2>
-            <button></button>
+        <div className={css.container}>
+            <div className={css.containerToday}>
+                <h2 className={css.text}>Today</h2>
+       <div>
+        <div className={css.progressContainer}>
+        <div className={css.progressBar}
+          style={{width: '60%'}}
+        ></div>
+        <div className={css.spanProgress}>
+          <span>0%</span>
+          <span>50%</span>
+          <span>100%</span>
+        </div>
+        <div className={css.elipce} style={{ left: `60%` }}>
+              <span className={css.interest}>{ '60%' }</span>                 
+                            
+        </div>
+      </div>
+     
+    </div>
+           </div> 
         </div>
     )
 };
