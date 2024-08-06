88e3b2f0e8b480d28672356d77b70516c7110d20
Vitaliia1109 <vitusechka@ukr.net>
2024-08-06 22:06:58 +0100

    redux

--- src/redux/water/selectors-water.js
+++
@@ -1,6 +0,0 @@
-// //Віталія
-// // export const selectWaterAmount = (state) => state.waterForm.waterAmount;
-// // export const selectDate = (state) => state.waterForm.date;
-// export const selectWaterStatus = (state) => state.waterForm.status;
-// export const selectWaterError = (state) => state.waterForm.error;
-// //Віталія
