dfaf4f7568cdafe23726f7421fc2f1da065f8c26
Yevhen <igrovoy88888@gmail.com>
2024-08-10 18:05:54 +0300

    Up

--- src/components/WelcomeSection/WelcomeSection.jsx
+++ src/components/WelcomeSection/WelcomeSection.jsx
@@ -1,9 +1,13 @@
 import { NavLink } from "react-router-dom";
 import css from "./WelcomeSection.module.css";
-import Logo from '../Logo/Logo.jsx'
+import Logo from "../Logo/Logo.jsx";
 import SectionWithLogo from "../SectionWithLogo/SectionWithLogo.jsx";
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/auth/selectors-auth.js";
 
 export const WelcomeSection = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn); // Проверка состояния авторизации
+
   return (
     <SectionWithLogo className={css.welcomeSection}>
       <p className={css.text}>Record daily water intake and track</p>
@@ -11,13 +15,15 @@ export const WelcomeSection = () => {
       <h1 className={css.title}>Water consumption tracker</h1>
 
       <div className={css.linkBox}>
-
         <NavLink to="/signup" className={css.linkSignUP}>
-
           Try tracker
         </NavLink>
-        <NavLink to="/signin" className={css.linkSignIn}>
-
+        <NavLink
+          to="/signin"
+          className={`${css.linkSignIn} ${isLoggedIn ? css.disabledLink : ""}`}
+          aria-disabled={isLoggedIn}
+          tabIndex={isLoggedIn ? -1 : 0}
+        >
           Sign In
         </NavLink>
       </div>
