7778559b480fb1b1d752e8314ffa76b7058b1c4d
Dmytro Popovskyi <popovskiy96@gmail.com>
2024-08-10 12:05:28 +0200

    update from main

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,8 +3,6 @@ import { WelcomeSection } from "../../components/WelcomeSection/WelcomeSection";
 import { AdvantagesSection } from "../../components/AdvantagesSection/AdvantagesSection";
 import { Helmet } from "react-helmet-async";
 import Page from "../../components/Page/Page";
-import WaterModal from "../../components/WaterModal/WaterModal.jsx";
-import Modal from "../../components/Modal/Modal.jsx";
 
 function HomePage() {
   return (
