5f67e2e8219ac8f8a8960b15851e3bb87fd16549
Hennadii Polupan <polupan.gennadiy@gmail.com>
2024-08-11 20:33:03 +0100

    button-add-blur-Hennadii

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,22 @@
 import css from "./Button.module.css";
 import clsx from "clsx";
 
-const Button = ({ children, onClick, styleType, className, type = 'submit' }) => {
+const Button = ({
+  children,
+  onClick,
+  styleType,
+  className,
+  type = "submit",
+}) => {
+  const handleClick = (e) => {
+    e.currentTarget.blur();
+    if (onClick) {
+      onClick();
+    }
+  };
   return (
     <button
-      onClick={onClick}
+      onClick={handleClick}
       className={clsx(css.button, className && className, {
         [css.transparent]: styleType === "transparent",
         [css.green]: styleType === "green",
