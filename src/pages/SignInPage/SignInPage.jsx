cb730de348d2e99121fbdd093aaebec611833e60
Yevhen <igrovoy88888@gmail.com>
2024-08-13 00:42:42 +0300

    Up

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,25 +1,35 @@
+import { useState, useEffect } from "react";
 import css from "./SignInPage.module.css";
 import { AdvantagesSection } from "../../components/AdvantagesSection/AdvantagesSection";
 import SignInForm from "../../components/SignInForm/SignInForm";
 import { Helmet } from "react-helmet-async";
-import screenWidth from "../../helpers/screenWidth.js";
-
 import Page from "../../components/Page/Page";
 import SectionWithLogo from "../../components/SectionWithLogo/SectionWithLogo.jsx";
 
 function SignInPage() {
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth > 1439);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth > 1439);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <>
       <Helmet>
         <title>AquaTrack</title>
       </Helmet>
-        <Page>
-            <SectionWithLogo className={css.signInSection}>
-                <SignInForm/>
-            </SectionWithLogo>
-            {screenWidth > 1439 && <AdvantagesSection/>}
-        </Page>
+      <Page>
+        <SectionWithLogo className={css.signInSection}>
+          <SignInForm />
+        </SectionWithLogo>
+        {isLargeScreen && <AdvantagesSection />}
+      </Page>
     </>
   );
 }
