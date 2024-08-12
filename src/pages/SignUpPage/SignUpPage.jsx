03c66a5f892283b8ea357c6f14d9ed30b085da12
Yevhen <igrovoy88888@gmail.com>
2024-08-13 01:33:38 +0300

    up

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,21 +1,32 @@
+import { useState, useEffect } from "react";
 import css from "./SignUpPage.module.css";
 import { AdvantagesSection } from "../../components/AdvantagesSection/AdvantagesSection";
 import { Helmet } from "react-helmet-async";
-
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
-import screenWidth from "../../helpers/screenWidth.js";
 import SectionWithLogo from "../../components/SectionWithLogo/SectionWithLogo.jsx";
 
 function SignUpPage() {
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth > 1439);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth > 1439);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <>
       <Helmet>
         <title>AquaTrack</title>
       </Helmet>
-          <SectionWithLogo className={css.sighnUpSection}>
-              <SignUpForm />
-          </SectionWithLogo>
-          {screenWidth > 1439 && <AdvantagesSection/>}
+      <SectionWithLogo className={css.sighnUpSection}>
+        <SignUpForm />
+      </SectionWithLogo>
+      {isLargeScreen && <AdvantagesSection />}
     </>
   );
 }
