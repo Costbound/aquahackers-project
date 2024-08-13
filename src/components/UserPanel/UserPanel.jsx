78fe21695eb354839e9921e93874dc1c0d804dbd
Lampdust <lampdust06@gmail.com>
2024-08-13 17:01:19 +0300

    добавление 3 точки в конце именни если слишком длинное

--- src/components/UserPanel/UserPanel.jsx
+++ src/components/UserPanel/UserPanel.jsx
@@ -5,12 +5,23 @@ import css from './UserPanel.module.css';
 
 const UserPanel = () => {
     const userName = useSelector(selectUserName);
-    const username = userName || 'User';
+
+    const formatUsername = (username) => {
+        // Получаем имя до первого пробела
+        let namePart = username.split(' ')[0];
+        
+        // Если имя длиннее 10 символов, обрезаем его и добавляем три точки
+        if (namePart.length > 10) {
+            return namePart.slice(0, 10) + '...';
+        }
+
+        return namePart;
+    };
 
     return (
         <div className={css.userPanel}>
             <h1 className={css.greeting}>
-                Hello<span className={css.panelUsername}>, {username}!</span>
+                Hello<span className={css.panelUsername}>, {formatUsername(userName || 'User')}!</span>
             </h1>
             <UserBar />
         </div>
