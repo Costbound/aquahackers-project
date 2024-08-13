39edbfd681d1a679ce25650580b763b4f7cdf834
Lampdust <lampdust06@gmail.com>
2024-08-13 17:12:45 +0300

    upd- добавление 3 точки к именни если слишком длинное

--- src/components/UserPanel/UserPanel.jsx
+++ src/components/UserPanel/UserPanel.jsx
@@ -2,26 +2,31 @@ import { useSelector } from 'react-redux';
 import { selectUserName } from '../../redux/userData/selectors-userData';
 import UserBar from '../UserBar/UserBar';
 import css from './UserPanel.module.css';
+import { useMediaQuery } from 'react-responsive';
 
 const UserPanel = () => {
     const userName = useSelector(selectUserName);
 
-    const formatUsername = (username) => {
-        // Получаем имя до первого пробела
-        let namePart = username.split(' ')[0];
-        
-        // Если имя длиннее 10 символов, обрезаем его и добавляем три точки
-        if (namePart.length > 10) {
-            return namePart.slice(0, 10) + '...';
-        }
+    // Определение размеров экрана
+    const isMobile = useMediaQuery({ maxWidth: 767 });
+    const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1439 });
+    const isDesktop = useMediaQuery({ minWidth: 1440 });
 
-        return namePart;
-    };
+    // Определение предела длины имени в зависимости от размера экрана
+    let maxLength = 18;
+    if (isMobile) maxLength = 10;
+    else if (isTablet) maxLength = 18;
+    else if (isDesktop) maxLength = 16;
+
+    // Обрезаем имя, если оно превышает установленный лимит
+    const truncatedUsername = userName && userName.length > maxLength 
+        ? `${userName.substring(0, maxLength)}...` 
+        : userName || 'User';
 
     return (
         <div className={css.userPanel}>
             <h1 className={css.greeting}>
-                Hello<span className={css.panelUsername}>, {formatUsername(userName || 'User')}!</span>
+                Hello<span className={css.panelUsername}>, {truncatedUsername}!</span>
             </h1>
             <UserBar />
         </div>
