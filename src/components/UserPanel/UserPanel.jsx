80a29453c6e3bbb22cb1aa1ba149cc965bb4258c
Iryna-Pavlyk <145845654+Iryna-Pavlyk@users.noreply.github.com>
2024-08-13 18:56:57 +0300

    fix UserPanel.jsx

--- src/components/UserPanel/UserPanel.jsx
+++ src/components/UserPanel/UserPanel.jsx
@@ -1,36 +1,37 @@
-import { useSelector } from 'react-redux';
-import { selectUserName } from '../../redux/userData/selectors-userData';
-import UserBar from '../UserBar/UserBar';
-import css from './UserPanel.module.css';
-import { useMediaQuery } from 'react-responsive';
+import { useSelector } from "react-redux";
+import { selectUserName } from "../../redux/userData/selectors-userData";
+import UserBar from "../UserBar/UserBar";
+import css from "./UserPanel.module.css";
+import { useMediaQuery } from "react-responsive";
 
 const UserPanel = () => {
-    const userName = useSelector(selectUserName);
+  const userName = useSelector(selectUserName);
 
-    // Определение размеров экрана
-    const isMobile = useMediaQuery({ maxWidth: 767 });
-    const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1439 });
-    const isDesktop = useMediaQuery({ minWidth: 1440 });
+  // Определение размеров экрана
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1439 });
+  const isDesktop = useMediaQuery({ minWidth: 1440 });
 
-    // Определение предела длины имени в зависимости от размера экрана
-    let maxLength = 18;
-    if (isMobile) maxLength = 10;
-    else if (isTablet) maxLength = 16;
-    else if (isDesktop) maxLength = 16;
+  // Определение предела длины имени в зависимости от размера экрана
+  let maxLength = 18;
+  if (isMobile) maxLength = 10;
+  else if (isTablet) maxLength = 16;
+  else if (isDesktop) maxLength = 14;
 
-    // Обрезаем имя, если оно превышает установленный лимит
-    const truncatedUsername = userName && userName.length > maxLength 
-        ? `${userName.substring(0, maxLength)}...` 
-        : userName || 'User';
+  // Обрезаем имя, если оно превышает установленный лимит
+  const truncatedUsername =
+    userName && userName.length > maxLength
+      ? `${userName.substring(0, maxLength)}...`
+      : userName || "User";
 
-    return (
-        <div className={css.userPanel}>
-            <h1 className={css.greeting}>
-                Hello<span className={css.panelUsername}>, {truncatedUsername}!</span>
-            </h1>
-            <UserBar />
-        </div>
-    );
-}
+  return (
+    <div className={css.userPanel}>
+      <h1 className={css.greeting}>
+        Hello<span className={css.panelUsername}>, {truncatedUsername}!</span>
+      </h1>
+      <UserBar />
+    </div>
+  );
+};
 
 export default UserPanel;
